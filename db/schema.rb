f12632c4a9df74074fd4b57748a63f491372ec37
GilmarBec <trabalhos4.5.1.4@gmail.com>
2018-09-27 19:12:31 -0300

    global changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180914004239) do
+ActiveRecord::Schema.define(version: 20180927192810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,21 @@ ActiveRecord::Schema.define(version: 20180914004239) do
     t.datetime "updated_at",        null: false
   end
 
+  create_table "solicitacoes", force: :cascade do |t|
+    t.string   "titulo"
+    t.integer  "user_id"
+    t.integer  "produto_id"
+    t.integer  "quantidade"
+    t.datetime "data_solicitacao"
+    t.datetime "data_devolucao"
+    t.string   "status"
+    t.string   "observacao"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["produto_id"], name: "index_solicitacoes_on_produto_id", using: :btree
+    t.index ["user_id"], name: "index_solicitacoes_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -52,4 +67,6 @@ ActiveRecord::Schema.define(version: 20180914004239) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "solicitacoes", "produtos"
+  add_foreign_key "solicitacoes", "users"
 end
